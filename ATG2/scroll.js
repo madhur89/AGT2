b0354ea417c28d2b77a656830de272dfcd8f8946
Madhur <68078506+madhur89@users.noreply.github.com>
2022-06-01 17:36:41 +0530

    Add files via upload

---
+++ ATG2/scroll.js
@@ -0,0 +1,12 @@
+var scene = new ScrollMagic.Scene({triggerElement: "#page2"})
+							
+.setClassToggle("#svg-text2", "slide-in-top")
+.addIndicators({name: "1 - add a class"}) 
+.addTo(controller);
+
+
+var scene = new ScrollMagic.Scene({triggerElement: "#page2"})
+
+.setClassToggle("#side-txt", "slide-in-fwd-left")
+.addIndicators({name: "1 - add a class"}) 
+.addTo(controller);
